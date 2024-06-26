c211321a72e6f40ae79b260922ad186462f1c950
syf0617 <siyuanfan97@yahoo.com>
2024-06-25 21:26:37 -0400

    Add files via upload

---
+++ C#_Basics/Program.cs
@@ -0,0 +1,11 @@
+﻿DateTime birthDate = new DateTime(2000,01, 01); 
+
+DateTime today = DateTime.Today;
+int daysOld = (today - birthDate).Days;
+
+int daysToNextAnniversary = 10000 - (daysOld % 10000);
+
+Console.WriteLine($"You are {daysOld} days old.");
+
+DateTime nextAnniversary = today.AddDays(daysToNextAnniversary);
+Console.WriteLine($"Your next 10,000 day anniversary will be on {nextAnniversary.ToShortDateString()}.");
