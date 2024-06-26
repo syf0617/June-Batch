c35e79c3c418989c3517359cc9080a3856940dba
syf0617 <siyuanfan97@yahoo.com>
2024-06-25 22:09:54 -0400

    Add files via upload

---
+++ C#_Basics/ArraysAndStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] initialArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+int[] copiedArray = new int[initialArray.Length];
+
+for (int i = 0; i < initialArray.Length; i++){
+    copiedArray[i] = initialArray[i];
+}
+
+Console.WriteLine("Initial Array:");
+
+foreach (int item in initialArray) {
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Copied Array:");
+
+foreach (int item in copiedArray){
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
